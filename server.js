b6e508c7e98c2f0b403256f38cdecbd63db4adeb
Riley Hardy <rileyjhardy@gmail.com>
2021-04-29 11:36:11 -0500

    added config

--- server.js
+++ server.js
@@ -18,14 +18,14 @@ connection.once('open', () => {
 
 if (process.env.NODE_ENV === 'production'){
 
-    app.use(express.static('client/build'));
+    app.use(express.static('frontend/build'));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client', "build", 'index.html'))
+        res.sendFile(path.join(__dirname, 'frontend', "build", 'index.html'))
 });
 }
 
-//
+
 
 const port = process.env.PORT || 5000;
 
@@ -34,4 +34,4 @@ app.use('/entry', entryRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
-});
+})
